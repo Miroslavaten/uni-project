936c2509dbcbb6fa08d2af73216d459022e371ae
Miroslavaten <mirislava.ten@gmail.com>
2025-05-10 00:36:35 +0600

    auth

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,11 +1,15 @@
 // src/routes.tsx
 import { Routes, Route } from "react-router-dom";
 import MainPage from "../pages/MainPage/MainPage";
+import {useAuth} from "../hooks/useAuth.ts";
+import {AuthForm} from "../components/Auth/AuthForm.tsx";
 
 const AppRoutes = () => {
+  const { user, loading } = useAuth();
+  if (loading) return <div>Загрузка авторизации...</div>;
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
+      {user ? <Route path="/" element={<MainPage />} /> : <Route path="/" element={<AuthForm/>}/>}
     </Routes>
   );
 };
